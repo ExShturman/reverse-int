59ca0556a66ab2e2dae84cba384561552755fa69
Alex <exshturman@gmail.com>
2020-03-15 17:40:54 +0400

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+        if(n<0){
+            n= -n;
+        }
+        let x = n.toString();
+        let y= x.split("").reverse("").join("");
+        return y;
 }
