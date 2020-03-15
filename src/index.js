af8f34984aac7f765fa0d4fe5cbc431df307ecb4
Alex <exshturman@gmail.com>
2020-03-15 17:43:00 +0400

    fix: fix markup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-module.exports = function reverse (n) {
-        if(n<0){
-            n= -n;
-        }
-        let x = n.toString();
-        let y= x.split("").reverse("").join("");
-        return y;
+module.exports = function reverse(n) {
+    if (n < 0) {
+        n = -n;
+    }
+    let x = n.toString();
+    let y = x.split("").reverse("").join("");
+    return y;
 }
